eff89144a8c696dbed65e41445b2718c4657aa45
raspuna <raspuna@gmail.com>
2022-04-27 15:26:28 -0700

    axios Pokemon api

--- week4/pokemon/src/App.js
+++ week4/pokemon/src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
 import axios from 'axios'
 import {useState} from 'react'
+import Button from 'react-bootstrap/Button'
 
 function App() {
   const [pokemons, setPokemons] = useState([])
-  axios.get('https://pokeapi.co/api/v2/pokemon')
-  .then((result) =>{
-    console.log(result.data.results)
-    setPokemons(result.data.results)
-  }).catch((error) => {
-    console.log(error)
-  })
+
+  const fetchPokemon = (e) => {
+    e.preventDefault();
+    console.log("pressed")
+    axios.get('https://pokeapi.co/api/v2/pokemon')
+    .then((result) =>{
+      console.log(result.data.results)
+      setPokemons(result.data.results)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+  const clearPokemon = (e) => {
+    e.preventDefault();
+    setPokemons([])
+  }
   return (
     <div className="App">
+      <Button onClick={fetchPokemon}>Fetch Pokemon</Button>
+      <Button variant="warning" onClick={clearPokemon}>Clear</Button>
       {
         pokemons.map((pokemon) => (
-          <div>{pokemon.name}</div>
+          <ul>
+            <li>{pokemon.name}</li>
+          </ul>
         ))
       }  
     </div>
