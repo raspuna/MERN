c782e02910b206543bdf9820e178a2212d6873e0
raspuna <raspuna@gmail.com>
2022-05-19 13:03:09 -0700

    Sensei

--- week7/Ninja/Ninja.js
+++ week7/Ninja/Ninja.js
@@ -11,11 +11,30 @@ class Ninja {
     showStats(){
         console.log(this)
     }
-    drinkShakes(){
+    drinkSake(){
         this.health += 10
+    }
+}
+class Sensei extends Ninja {
+    constructor(ninjaName) {
+        super(ninjaName, 200)
+        this.speed = 10
+        this.strength = 10
+    }
+    speakWisdom(){
+        super.drinkSake()
+        console.log("What one programmer can do in one month, two programmers can do in two months")
     }
 }
 
 const sakura = new Ninja("Sakura", 30)
-sakura.drinkShakes()
+sakura.drinkSake()
 sakura.showStats()
+
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
